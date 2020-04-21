42a5e920a331fe92da95ca8ce09f5a9b4c6a1585
Maciej Mieczakowski <maciej.mieczakowski@gmail.com>
2020-04-21 21:41:04 +0200

    //Second version, static method instead (see first commit for 1st version)|

--- src/Interval.java
+++ src/Interval.java
@@ -1,47 +1,18 @@
 public class Interval {
-    private int evenSum;
-    private final int min;
-    private final int max;
-    private final int[] evenValues;
-    private int evenCounter;
 
-    public Interval(int min, int max) {
-
-        this.min = min;
-        this.max = max;
-        this.evenSum = 0;
-        //all possibilities to check in order to create appropriate exact table size - my goal to not create bigger table size, I know it's worthless ;)
-        if (((max - min) % 2 == 0) && ((max % 2 != 0))) {
-            this.evenValues = new int[((max - min) / 2)];
-        } else {
-            this.evenValues = new int[((max - min) / 2) + 1];
-        }
-        this.evenCounter = 0;
-
-    }
-
-    public void displayEvenValues() {
-        System.out.println("\nAll even values for interval [" + min + "," + max + "]:");
-        for (int evenValue : evenValues) {
-            if (evenCounter != 0)
-                System.out.print(evenValue + " ");
-        }
-    }
-
-    public void displaySumOfEvenValues() {
-        System.out.println("\nSum of all even values for interval [" + min + "," + max + "]:");
-        System.out.println(evenSum);
-    }
-
-    public void lookForEvenValues() {
-
+    public static void showEvenValuesForIntervalAndTheirSum(int min, int max) {
+        int evenSum = 0;
         for (int i = min; i <= max; i++) {
-            if ((i % 2) == 0) {
-                evenValues[evenCounter] = i;
-                evenCounter++;
+            if (isEven(i)) {
+                System.out.println(i + " is even value");
                 evenSum += i;
             }
         }
+        System.out.println("Sum of all above is: " + evenSum);
+    }
+
+    public static boolean isEven(int i) {
+        return ((i % 2) == 0);
     }
 }
 
