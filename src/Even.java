f0a467f5ff6e052e301515fb9f4acc80fbadf9e9
Maciej Mieczakowski <maciej.mieczakowski@gmail.com>
2020-04-21 21:13:11 +0200

    //Not necessary to use tables, it was all my goal to do this that way. It could be much more simplified. Simplified in second commit

---
+++ src/Even.java
@@ -0,0 +1,56 @@
+public class Even {
+    public static void main(String[] args) {
+
+        //Main goal
+        Interval interval1 = new Interval(0, 100);
+        interval1.lookForEvenValues();
+        interval1.displayEvenValues();
+        interval1.displaySumOfEvenValues();
+
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        //All below is to check  my own goal
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        Interval interval2 = new Interval(1, 11);
+        interval2.lookForEvenValues();
+        interval2.displayEvenValues();
+        interval2.displaySumOfEvenValues();
+
+        Interval interval4 = new Interval(0, 11);
+        interval4.lookForEvenValues();
+        interval4.displayEvenValues();
+        interval4.displaySumOfEvenValues();
+
+        Interval interval3 = new Interval(-10, -5);
+        interval3.lookForEvenValues();
+        interval3.displayEvenValues();
+        interval3.displaySumOfEvenValues();
+
+        Interval interval5 = new Interval(-10, -4);
+        interval5.lookForEvenValues();
+        interval5.displayEvenValues();
+        interval5.displaySumOfEvenValues();
+
+        Interval interval6 = new Interval(-2, 11);
+        interval6.lookForEvenValues();
+        interval6.displayEvenValues();
+        interval6.displaySumOfEvenValues();
+
+        Interval interval7 = new Interval(-1, 11);
+        interval7.lookForEvenValues();
+        interval7.displayEvenValues();
+        interval7.displaySumOfEvenValues();
+
+        Interval interval8 = new Interval(-9, -3);
+        interval8.lookForEvenValues();
+        interval8.displayEvenValues();
+        interval8.displaySumOfEvenValues();
+
+        Interval interval9 = new Interval(3, 3);
+        interval9.lookForEvenValues();
+        interval9.displayEvenValues();
+        interval9.displaySumOfEvenValues();
+
+    }
+
+}
+
